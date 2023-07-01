0f152e792406c922487268d7cd23c36331cef64a
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-07-01 22:02:03 +0200

    feat: add mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,62 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+
+const providedUrlMock = '/posts/1';
+const baseUrlMock = 'https://jsonplaceholder.typicode.com';
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+    jest.restoreAllMocks();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const axiosClient = {
+      get: jest.fn().mockResolvedValueOnce({ data: 'resolved value' }),
+    };
+
+    (axios.create as jest.Mock).mockReturnValueOnce(axiosClient);
+
+    await throttledGetDataFromApi(providedUrlMock);
+
+    jest.runAllTimers();
+
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL: baseUrlMock,
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const axiosClient = {
+      get: jest.fn().mockResolvedValueOnce({ data: 'resolved value' }),
+    };
+
+    (axios.create as jest.Mock).mockReturnValueOnce(axiosClient);
+
+    await throttledGetDataFromApi(providedUrlMock);
+
+    await Promise.resolve();
+
+    jest.runAllTimers();
+
+    expect(axiosClient.get).toHaveBeenCalledWith(providedUrlMock);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const axiosClient = {
+      get: jest.fn().mockResolvedValueOnce({ data: 'other resolved value' }),
+    };
+
+    (axios.create as jest.Mock).mockReturnValueOnce(axiosClient);
+
+    const result = await throttledGetDataFromApi(providedUrlMock);
+
+    expect(result).toEqual('other resolved value');
   });
 });
